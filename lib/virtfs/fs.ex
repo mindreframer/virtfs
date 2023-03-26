435501269d4d1f0e84c230b40f0bc02f96281969
Roman Heinrich <roman.heinrich@gmail.com>
2023-03-26 23:30:54 +0200

    Feat: simplify Virtfs.FS by removing the backend field

--- lib/virtfs/fs.ex
+++ lib/virtfs/fs.ex
@@ -1,33 +1,22 @@
 defmodule Virtfs.FS do
   alias Virtfs.FS
-  alias Virtfs.Backend
-
-  @type kind :: :real | :virt
   use TypedStruct
 
   typedstruct do
     @typedoc "Virtual Filesystem"
 
-    field(:kind, kind, default: :virt)
-    field(:backend, Virtfs.Behaviour, default: Backend)
     field(:cwd, String.t(), default: "/")
     field(:files, map())
   end
 
   def init(opts \\ []) do
-    type = Keyword.get(opts, :type, :virt)
     path = Keyword.get(opts, :path, "/")
-    backend = backend_for(type)
 
     %FS{
-      kind: type,
       files: %{
         "/" => Virtfs.File.new_dir("/")
       },
-      cwd: path,
-      backend: backend
+      cwd: path
     }
   end
-
-  def backend_for(_), do: Backend
 end
